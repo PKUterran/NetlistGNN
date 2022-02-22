d520f484493fdf42c09ae0031b0c95bc8b1f8dbf
PKUterran <374308602@qq.com>
2022-02-22 22:43:02 +0800

    images

--- data/DIT.py
+++ data/DIT.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,12 +7,18 @@ from PIL import Image
 from typing import Tuple, List
 
 
+INPUT_IMAGE = 'input'
+OUTPUT_IMAGE = 'output'
+EVAL_IMAGE = 'eval'
+
+
 def generate_rgb_init(h_capacity: np.ndarray, v_capacity: np.ndarray,
                       h_net_density: np.ndarray, v_net_density: np.ndarray,
-                      pin_density: np.ndarray
+                      pin_density: np.ndarray, labels: np.ndarray
                       ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     len_x = h_capacity.shape[0]
     len_y = h_capacity.shape[1]
+    scale = 1
 
     # h_capacity = np.zeros_like(h_capacity)
     # v_capacity = np.zeros_like(v_capacity)
@@ -22,23 +29,33 @@ def generate_rgb_init(h_capacity: np.ndarray, v_capacity: np.ndarray,
     r_channel = np.zeros(shape=[len_x * 2, len_y * 2], dtype=np.float)
     g_channel = np.zeros_like(r_channel)
     b_channel = np.zeros_like(r_channel)
-    g_channel[1::2, ::2] = h_capacity + h_net_density
+
+    r_channel[::2, ::2] = labels
+    r_channel[1::2, ::2] = labels
+    r_channel[::2, 1::2] = labels
+    r_channel[1::2, 1::2] = labels
+    g_channel[1::2, ::2] = h_capacity + h_net_density * scale
     b_channel[1::2, ::2] = h_capacity
-    g_channel[::2, 1::2] = v_capacity + v_net_density
+    g_channel[::2, 1::2] = v_capacity + v_net_density * scale
     b_channel[::2, 1::2] = v_capacity
-    g_channel[1::2, 1::2] = h_net_density + v_net_density
+    g_channel[1::2, 1::2] = h_net_density * scale + v_net_density * scale
     b_channel[1::2, 1::2] = pin_density
-    return r_channel, g_channel * 64, b_channel * 6
+    return r_channel * 64, g_channel * 64, b_channel * 6
 
 
-def dump_data(dir_name: str, raw_dir_name: str, given_iter, bin_x: float = 32, bin_y: float = 40, use_tqdm=True
-              ):
+def dump_data(dir_name: str, raw_dir_name: str, given_iter, index: int, hashcode: str,
+              bin_x: float = 32, bin_y: float = 40, force_save=False, use_tqdm=True):
+    if not force_save and os.path.exists(f'{dir_name}/{INPUT_IMAGE}.png'):
+        return
+
     h_capacity = np.load(f'{raw_dir_name}/iter_{given_iter}_bad_cmap_h.npy')
     v_capacity = np.load(f'{raw_dir_name}/iter_{given_iter}_bad_cmap_v.npy')
     h_net_density = np.load(f'{raw_dir_name}/hdm.npy')
     v_net_density = np.load(f'{raw_dir_name}/vdm.npy')
     xdata = np.load(f'{dir_name}/xdata_{given_iter}.npy')
     ydata = np.load(f'{dir_name}/ydata_{given_iter}.npy')
+    labels = np.load(f'{dir_name}/iter_{given_iter}_grid_label_full_{hashcode}_.npy')
+    labels = labels[:, :, index]
 
     len_x = h_capacity.shape[0]
     len_y = h_capacity.shape[1]
@@ -55,7 +72,7 @@ def dump_data(dir_name: str, raw_dir_name: str, given_iter, bin_x: float = 32, b
     r_channel, g_channel, b_channel = generate_rgb_init(
         h_capacity=h_capacity, v_capacity=v_capacity,
         h_net_density=h_net_density, v_net_density=v_net_density,
-        pin_density=pin_density
+        pin_density=pin_density, labels=labels
     )
     # print(len_x, len_y)
     # print(np.max(xdata) / bin_x, np.max(ydata) / bin_y)
@@ -67,18 +84,50 @@ def dump_data(dir_name: str, raw_dir_name: str, given_iter, bin_x: float = 32, b
     print(np.max(b_channel))
     # print(pin_density)
 
-    im = Image.new('RGB', (len_x, len_y))
+    im1 = Image.new('RGB', (len_x, len_y))
+    im2 = Image.new('RGB', (len_x, len_y))
+    im3 = Image.new('RGB', (len_x, len_y))
     for x in range(len_x):
         for y in range(len_y):
-            im.putpixel((x, y), (
+            im1.putpixel((x, y), (
+                0,
+                min(int(g_channel[x, y]), 255),
+                min(int(b_channel[x, y]), 255)
+            ))
+            im2.putpixel((x, y), (
                 min(int(r_channel[x, y]), 255),
                 min(int(g_channel[x, y]), 255),
                 min(int(b_channel[x, y]), 255)
             ))
-    im.save(f'{dir_name}/image.png')
+            im3.putpixel((x, y), (
+                min(int(r_channel[x, y]), 255),
+                0,
+                0
+            ))
+    im1.save(f'{dir_name}/{INPUT_IMAGE}.png')
+    im2.save(f'{dir_name}/{OUTPUT_IMAGE}.png')
+    im3.save(f'{dir_name}/{EVAL_IMAGE}.png')
 
 
-def load_data(dir_name: str, given_iter, index: int, hashcode: str,
-              graph_scale: int = 1000, bin_x: float = 40, bin_y: float = 32, force_save=False, use_tqdm=True
-              ):
+def collect_data(dirs: List[str], storage: str, part_x=64, part_y=64, clear_files=False):
+    if not os.path.isdir(storage):
+        os.mkdir(storage)
+    if clear_files:
+        for file in os.listdir(storage):
+            os.remove(f'{storage}/{file}')
+
+    for pre in [INPUT_IMAGE, OUTPUT_IMAGE, EVAL_IMAGE]:
+        cnt = 0
+        for dir_name in dirs:
+            im = Image.open(f'{dir_name}/{pre}.png')
+            w, h = im.width, im.height
+            max_x, max_y = int(w / part_x), int(h / part_y)
+            for x in range(max_x):
+                for y in range(max_y):
+                    im_xy = im.crop((x * part_x, y * part_y, (x + 1) * part_x, (y + 1) * part_y))
+                    im_xy.save(f'{storage}/{pre}_{cnt}.png')
+                    cnt += 1
+
+
+def load_data(dir_name: str, use_tqdm=True):
     pass
---
+++ net/GanRoute.py
@@ -0,0 +1,85 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.datasets import MNIST
+import os
+
+if not os.path.exists('./dc_img'):
+    os.mkdir('./dc_img')
+
+
+def to_img(x):  # 将vector转换成矩阵
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+
+num_epochs = 100
+batch_size = 128
+learning_rate = 1e-3
+
+img_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = MNIST('./data', transform=img_transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+class ImageAutoEncoder(nn.Module):
+    def __init__(self):
+        super(ImageAutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+            nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+            nn.ReLU(True),
+            nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.Tanh()  # 将输出值映射到-1~1之间
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = autoencoder().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
+                             weight_decay=1e-5)
+
+for epoch in range(num_epochs):
+    for data in dataloader:
+        img, _ = data  # img是一个b*channel*width*height的矩阵
+        img = Variable(img).cuda()
+        # ===================forward=====================
+        output = model(img)
+        a = img.data.cpu().numpy()
+        b = output.data.cpu().numpy()
+        loss = criterion(output, img)
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    # ===================log========================
+    print('epoch [{}/{}], loss:{:.4f}'
+          .format(epoch+1, num_epochs, loss.data[0]))
+    if epoch % 10 == 0:
+        pic = to_img(output.cpu().data)  # 将decoder的输出保存成图像
+        save_image(pic, './dc_img/image_{}.png'.format(epoch))
+
+torch.save(model.state_dict(), './conv_autoencoder.pth')
