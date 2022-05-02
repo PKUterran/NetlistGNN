83db84739b9e8c96aad53ab7f5b8190c3fd7ad55
PKUterran <374308602@qq.com>
2022-05-02 16:43:00 +0000

    log.script

--- log/script_compare.py
+++ log/script_compare.py
@@ -9,7 +9,7 @@ from typing import Dict, Tuple, List, Any
 COLOR_SET = ['red', 'green', 'blue', 'purple', 'orange', 'black']
 
 
-def get_tendency(logs: List[Dict[str, Any]]) -> Dict[str, Any]:
+def get_tendency(logs: List[Dict[str, Any]]) ->  Tuple[Dict[str, Any], int]:
     list_epoch = [log['epoch'] for log in logs]
     list_train_time = [log['train_time'] / 5 for log in logs]
     list_eval_time = [log['eval_time'] for log in logs]
@@ -44,6 +44,7 @@ def get_tendency(logs: List[Dict[str, Any]]) -> Dict[str, Any]:
     list_test_grid_index_kendalltau_rho = [log['test_grid_index_kendalltau_rho'] for log in logs]
     # list_test_grid_index_mae = [log['test_grid_index_mae'] for log in logs]
     # list_test_grid_index_rmse = [log['test_grid_index_rmse'] for log in logs]
+    best_epoch = -1
 
     return {
         'time': list_train_time,
@@ -58,7 +59,7 @@ def get_tendency(logs: List[Dict[str, Any]]) -> Dict[str, Any]:
         'pearson (grid index)': list_test_grid_index_pearson_rho,
         'spearmanr (grid index)': list_test_grid_index_spearmanr_rho,
         'kendalltau (grid index)': list_test_grid_index_kendalltau_rho,
-    }
+    }, best_epoch
 
 
 def plt_compare(name_values: Dict[str, List[float]], fig_path: str):
@@ -72,8 +73,18 @@ def plt_compare(name_values: Dict[str, List[float]], fig_path: str):
 
 
 PLT_TUPLES = [
-    ('Ours', 'superblue19/hyper.json'),
-    ('Ours new 1', 'superblue19/hyper-test.json'),
+#     ('(8,10)', 'superblue19/hyper(8,10).json'),
+#     ('(16,20)', 'superblue19/hyper(16,20).json'),
+#     ('(32,40)', 'superblue19/hyper.json'),
+#     ('(64,80)', 'superblue19/hyper(64,80).json'),
+#     ('(128,160)', 'superblue19/hyper(128,160).json'),
+    
+    ('2', 'superblue19/hyper(cap2).json'),
+    ('5', 'superblue19/hyper.json'),
+    ('10', 'superblue19/hyper(cap10).json'),
+    ('20', 'superblue19/hyper(cap20).json'),
+#     ('Ours', 'superblue19/hyper.json'),
+#     ('Ours new 1', 'superblue19/hyper-test.json'),
 #     ('Ours new 2', 'superblue19/hyper-test2.json'),
 #     ('Ours (o. geom.)', 'superblue19/hyper-geom.json'),
 #     ('Ours (o. geom.) new 1', 'superblue19/hyper-geom-test.json'),
@@ -81,7 +92,7 @@ PLT_TUPLES = [
 #     ('Ours (o. geom.) CF', 'superblue19/hyper-geom-CF.json'),
 #     ('Ours (o. geom.)', 'superblue16/hyper-geom.json'),
 #     ('LHNN', 'superblue16/LHNN.json'),
-    ('Ours (o. topo.)', 'superblue19/hyper-topo.json'),
+#     ('Ours (o. topo.)', 'superblue19/hyper-topo.json'),
 #     ('Ours (o. topo.) new 1', 'superblue19/hyper-topo-test.json'),
 #     ('Ours (o. topo.) new 2', 'superblue19/hyper-topo-test2.json'),
     # ('SAGE', 'superblue19/SAGE.json'),
@@ -103,7 +114,7 @@ if __name__ == '__main__':
         try:
             with open(path) as fp:
                 d = json.load(fp)
-            ret = get_tendency(d)
+            ret, be = get_tendency(d)
             ds_p[name] = ret['pearson (grid index)']
             ds_s[name] = ret['spearmanr (grid index)']
             ds_k[name] = ret['kendalltau (grid index)']
@@ -114,7 +125,7 @@ if __name__ == '__main__':
             for k, v in ret.items():
                 if len(v) == 0:
                     continue
-                print(f'\t{k}: {v[-1]:.3f}')
+                print(f'\t{k}: {v[be]:.3f}')
         except FileNotFoundError:
             print(f'For {name}: not found')
     plt_compare(ds_p, fig_path='figures/compare_pearson.png')
